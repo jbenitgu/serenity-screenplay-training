9344ec2d765e82a5c92d98bbe971c4748b1b5463
Cesar Alcantara <calcantv@emeal.nttdata.com>
2023-01-17 21:10:03 -0500

    fix(mobile): Reformat code

--- src/test/java/com/example/userinterfaces/screens/LoginScreen.java
+++ src/test/java/com/example/userinterfaces/screens/LoginScreen.java
@@ -4,7 +4,8 @@ import io.appium.java_client.AppiumBy;
 import net.serenitybdd.screenplay.targets.Target;
 
 public class LoginScreen {
-    public static final Target BTN_CLOSE = Target.the("Boton cerrar").locatedForAndroid(AppiumBy.xpath("//android.widget.ImageButton[@content-desc=\"Close\"]")).
-            locatedForIOS(AppiumBy.xpath("xxx"));
+    public static final Target BTN_CLOSE = Target.the("Boton cerrar")
+            .locatedForAndroid(AppiumBy.xpath("//android.widget.ImageButton[@content-desc=\"Close\"]"))
+            .locatedForIOS(AppiumBy.xpath("xxx"));
 
 }
